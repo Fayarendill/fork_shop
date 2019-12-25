0abcbf8d95ef99bcdc5536a72b6d9a2ce744bc8d
Fayarendill <voinov.forever@gmail.com>
2019-12-25 02:47:02 +0200

    semi working queries

--- src/FS/DataBase.hs
+++ src/FS/DataBase.hs
@@ -13,7 +13,9 @@
 {-# LANGUAGE UndecidableInstances       #-}
 
 module FS.DataBase
-  (
+  (runDb, getOrderByID, myDefConnInfo,
+   getForkByID, createFork, createFork',
+   Fork(Fork)
   ) where
 
 import           Control.Applicative
@@ -47,57 +49,72 @@ User
     phone T.PhoneNumber
     deriving Show
 Order
-    order_id T.OrderId
     customer_id T.CustomerId
     product_id T.ProductId
     quantity T.Quantity
     order_status T.OrderStatus
     status_changed_at T.OrderStatusChangedDate
+    deriving Show
 Fork
-    fork_id T.ProductId
     name T.ProductName
     merchant_id T.MerchantId
     price T.Price
     amount T.Quantity
     product_status T.ProductStatus
+    deriving Show
 |]
 
--- share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
--- Customer
---     customer_id T.CustomerUUID default=uuid_generate_v4()
---     UniqueCustomerUUID customer_id
---     first_name String
---     last_name String
---     deriving Show
--- CustomerInfo
---     info_id T.CustomerId
---     email T.Email
---     country_code T.CountryCode
---     phone T.PhoneNumber
---     deriving Show
--- OrderItems
---     order_id T.OrderId
---     product_id T.ProductId
---     quantity T.Quantity
--- Order
---     oId T.OrderId
---     user_id T.CustomerId
---     oStatus T.OrderStatus
---     status_changed_at T.OrderStatusChangedDate
--- Product
---     pId T.ProductId
---     name T.ProductName
---     merchant_id T.MerchantId
---     price T.Price
---     amount T.Quantity
---     pStatus T.ProductStatus
--- |]
+myDefConnInfo :: MySQL.ConnectInfo
+myDefConnInfo = defaultConnectInfo {
+  connectHost = "77.47.192.87",
+  connectPort = 33321,
+  connectUser = "ka7517",
+  connectPassword = "123456",
+  connectDatabase = "ka7517"
+  }
+             --     mkMySQLConnectInfo "77.47.192.87:33321" "ka7517" "123456" "ka7517"
 
-runDb :: MySQL.ConnectInfo -> SqlPersistT (ResourceT IO) a -> IO a
-runDb connInfo query = runResourceT . withMySQLConn connInfo . runSqlConn $ query
+-- inBackend :: ReaderT SqlBackend (NoLoggingT (ResourceT IO)) a-> IO a
+inBackend :: SqlPersistT (NoLoggingT (ResourceT IO)) a -> IO a
+inBackend action = runStdoutLoggingT $ withMySQLPool myDefConnInfo 10 $ \pool -> liftIO $ do
+  flip runSqlPersistMPool pool $ do
+    runMigration migrateAll
+    action
 
-getOrderInfoByID :: MySQL.ConnectInfo -> T.OrderId -> IO [Entity T.OrderInfo]
-getOrderInfoByID oid connInfo = (runDb connInfo $ select)
+getOrderByID :: Int -> IO (Maybe Order)
+getOrderByID i = inBackend . get $ (toSqlKey (fromInteger.toInteger $ i) :: OrderId)
+
+createOrder :: Order -> IO ()
+createOrder (Order cid pid q _ _) = inBackend $ do
+  now <- liftIO getCurrentTime
+  orderId <- insert $ Order cid pid q  T.WaitingForPayment (T.getDay now)
+  order <- get orderId
+  liftIO $ print order
+
+getForkByID :: Int -> IO (Maybe Fork)
+getForkByID i = inBackend . get $ (toSqlKey (fromInteger.toInteger $ i) :: ForkId)
+
+createFork :: Fork -> IO ()
+createFork fork@(Fork name mid price amount status) = inBackend $ do
+  now <- liftIO getCurrentTime
+  orderId <- insert $ fork
+  order <- get orderId
+  liftIO $ print order
+
+createFork' :: Fork -> IO ()
+createFork' fork@(Fork name mid price amount status) = runDb myDefConnInfo $ do
+  now <- liftIO getCurrentTime
+  orderId <- insert $ fork
+  order <- get orderId
+  liftIO $ print order
+
+runDb :: MySQL.ConnectInfo -> SqlPersistT (LoggingT IO) a -> IO a
+runDb connInfo query = runStdoutLoggingT . withMySQLConn connInfo . runSqlConn $ query
+
+-- getOrderByID :: MySQL.ConnectInfo -> T.OrderId -> IO [Entity Order]
+-- getOrderByID connInfo oid = (runDb connInfo $ select)
+--   where
+--     select = rawSql "select ?? from order where order_id=?" [PersistInt64 .fromInteger.toInteger $ oid]
 
 --blaze = S.html . renderHtml
 
