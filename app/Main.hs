255802fa5430bcc06bf97e8b838c2f595a96d574
Fayarendill <voinov.forever@gmail.com>
2019-12-27 21:46:10 +0200

    fix

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
+import           Control.Monad.Logger
+import           Data.Error
+import           Enum.Print
+import           FS.DataBase
 import           FS.Web
+import qualified Types                as T
 
 main :: IO ()
-main = test
+main = forkShop
+-- main = do
+--   let fork  = (Fork "fork1" 0 22 10 T.RunningLow)
+-- --  let order = (Fork "test" 0 8822 1 T.RunningLow)
+--   let user  = (User T.nilCustomerUUIDStr "john" "Doe" "example@mail.com" 38 888)
+--   createFork fork
+--   deleteForkByID 14
+--   --updateForkByID 15 fork
+
+
--- src/FS/Web.hs
+++ src/FS/Web.hs
@@ -1,12 +1,55 @@
 {-# LANGUAGE OverloadedStrings #-}
-
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
 module FS.Web
-  ( test
+  ( forkShop
   ) where
 
-import           Web.Scotty
+-- import           Web.Scotty
+-- import           Text.Hamlet
+import qualified Text.Cassius as SC
+import qualified Text.Lucius  as SL
+import           Yesod
 
-test :: IO ()
-test = scotty 3000 $ do
-  get "/" $ do
-    html "Hello World!"
+data ForkShop = ForkShop
+
+mkYesod "ForkShop" [parseRoutes|
+/ HomeR GET
+/market MarketR GET
+/orders OrdersR GET
+|]
+
+instance Yesod ForkShop
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout $ do
+  -- addStylesheet ($(SC.cassiusFile "cassius/home-menu.cassius") ())
+  -- addStylesheet $ StaticR css_bootstrap_css
+  -- toWidget $(SC.cassiusFile "templates/home-menu.cassius")
+  toWidget $(SL.luciusFileReload "templates/home-menu.lucius")
+  $(whamletFile "templates/home-menu.hamlet")
+  where
+    isLoggedIn = True
+-- getHomeR = defaultLayout [whamlet|
+-- This is home!
+--     <a href=@{MarketR}>Go to market!
+--     <a href=@{OrdersR}>Go to orders!|]
+
+getMarketR :: Handler Html
+getMarketR = defaultLayout [whamlet| This market!
+<a href=@{HomeR}>Go to home!|]
+
+getOrdersR :: Handler Html
+getOrdersR = defaultLayout [whamlet| This orders!
+<a href=@{HomeR}>Go to home!|]
+
+
+
+forkShop :: IO ()
+forkShop = warp 3000 ForkShop
+
+-- test :: IO ()
+-- test = scotty 3000 $ do
+--   get "/" $ do
+--     html "Hello World!"
